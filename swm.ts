bc4669d301c26e8f3afadd70190fd92fc130aacd
aalfiann <aalfiann@gmail.com>
2025-04-12 21:15:56 +0700

    - update version

--- swm.ts
+++ swm.ts
@@ -1,5 +1,5 @@
 /**
- * Service Worker Manager TS - v1.4.0
+ * Service Worker Manager TS - v1.4.1
  * https://github.com/aalfiann/swm
  */
 import { FirebaseApp, initializeApp, FirebaseOptions } from "firebase/app";
@@ -219,7 +219,7 @@ export class APIFallbacks {
 }
 
 export class ServiceWorkerManager {
-  static VERSION: string = '1.4.0';
+  static VERSION: string = '1.4.1';
   static initialized: boolean = false;
   static _lastOptions: ServiceWorkerManagerOptions | null = null;
 
